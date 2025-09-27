1aa6130083835308279ea52f4fef3957a12dbf28
RenatNig <RenatNig>
2025-09-27 15:20:43 +0300

    Early work start

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
-//var myHeading = document.querySelector("title");
-//myHeading.textContent = "Hello world!";
+
+
+const url = "http://127.0.0.1:8000/";
+try {
+    const response =  fetch(url);
+    
+
+    const result =  response.json();
+    console.log(result);
+} catch (error) {
+    console.error(error.message);
+}
+
+var mainInput = document.getElementById("mainInput");
+mainInput.textContent = "Hello world!";
